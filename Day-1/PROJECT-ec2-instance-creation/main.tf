bb90a2a31aebf13e429788cdc6bcbd9d7993ecf6
Santhoshbabu023 <umasanthosh81@gmail.com>
2026-01-12 20:03:17 +0530

    updated conf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-07ff62358b87c7116"  
-    instance_type = "t2.micro"
-    subnet_id = "subnet-0b43a69695dca3092"
-    key_name = "Terraform"
+    ami           =   
+    instance_type = 
+    subnet_id = 
+    key_name = 
 }
